8a8745f8df03cc9e26b6f1a4f55bff98bee1153f
Belinda <bmdebruyn@gmail.com>
2025-05-28 14:03:57 -0600

    set up sunburst chart

---
+++ src/aps_index.js
@@ -0,0 +1,293 @@
+(function () {
+    /**
+     * Template for the Styling Panel (APS) of the Funnel3D widget.
+     */
+    let template = document.createElement('template');
+    template.innerHTML = `
+        <form id="form">
+        <legend style="font-weight: bold;font-size: 18px;"> Font </legend>
+        <table>
+            <tr>
+                <td>Chart Title</td>
+            </tr>
+            <tr>
+                <td><input id="chartTitle" type="text"></td>
+            </tr>
+            <tr>
+                <table>
+                    <tr>
+                        <td>Size</td>
+                        <td>Font Style</td>
+                        <td>Alignment</td>
+                        <td>Color</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select id="titleSize">
+                                <option value="10px">10</option>
+                                <option value="12px">12</option>
+                                <option value="14px">14</option>
+                                <option value="16px">16</option>
+                                <option value="18px" selected>18</option>
+                                <option value="20px">20</option>
+                                <option value="22px">22</option>
+                                <option value="24px">24</option>
+                                <option value="32px">32</option>
+                                <option value="48px">48</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select id="titleFontStyle">
+                                <option value="normal">Normal</option>
+                                <option value="bold" selected>Bold</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select id="titleAlignment">
+                                <option value="left" selected>Left</option>
+                                <option value="center">Center</option>
+                                <option value="right">Right</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input id="titleColor" type="color" value="#333333">
+                        </td>
+                    </tr>
+                </table>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td>Chart Subtitle</td>
+            </tr>
+            <tr>
+                <td><input id="chartSubtitle" type="text"></td>
+            </tr>
+            <tr>
+                <table>
+                    <tr>
+                        <td>Size</td>
+                        <td>Font Style</td>
+                        <td>Alignment</td>
+                        <td>Color</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select id="subtitleSize">
+                                <option value="10px">10</option>
+                                <option value="12px" selected>12</option>
+                                <option value="14px">14</option>
+                                <option value="16px">16</option>
+                                <option value="18px">18</option>
+                                <option value="20px">20</option>
+                                <option value="22px">22</option>
+                                <option value="24px">24</option>
+                                <option value="32px">32</option>
+                                <option value="48px">48</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select id="subtitleFontStyle">
+                                <option value="normal" selected>Normal</option>
+                                <option value="italic">Italic</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select id="subtitleAlignment">
+                                <option value="left" selected>Left</option>
+                                <option value="center">Center</option>
+                                <option value="right">Right</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input id="subtitleColor" type="color" value="#666666">
+                        </td>
+                    </tr>
+                </table>
+            </tr>
+        </table>
+        <legend style="font-weight: bold;font-size: 18px;"> Number Formatting </legend>
+        <table>
+            <tr>
+                <td>Scale Format</td>
+            </tr>
+            <tr>
+                <td>
+                    <select id="scaleFormat">
+                        <option value="unformatted" selected>Unformatted</option>
+                        <option value="k">Thousands (k)</option>
+                        <option value="m">Millions (m)</option>
+                        <option value="b">Billions (b)</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Decimal Places</td>
+            </tr>
+            <tr>
+                <td>
+                    <select id="decimalPlaces">
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2" selected>2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
+        <input type="submit" style="display:none;">
+        </form>
+    `;
+
+    /**
+     * Custom Web Component for the Styling Panel (APS) of the Sunburst widget.
+     * @extends HTMLElement
+     */
+    class SunburstAps extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({ mode: 'open' });
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
+            this._shadowRoot.getElementById('form').addEventListener('submit', this._submit.bind(this));
+            this._shadowRoot.getElementById('titleSize').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('titleFontStyle').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('titleAlignment').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('titleColor').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('subtitleSize').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('subtitleFontStyle').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('subtitleAlignment').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('subtitleColor').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('scaleFormat').addEventListener('change', this._submit.bind(this));
+            this._shadowRoot.getElementById('decimalPlaces').addEventListener('change', this._submit.bind(this));
+        }
+
+        /**
+         * Handles the form submissions and dispatches a 'propertiesChanged' event.
+         * @param {Event} e - The form submission event.
+         */
+        _submit(e) {
+            e.preventDefault();
+            this.dispatchEvent(new CustomEvent('propertiesChanged', {
+                detail: {
+                    properties: {
+                        chartTitle: this.chartTitle,
+                        titleSize: this.titleSize,
+                        titleFontStyle: this.titleFontStyle,
+                        titleAlignment: this.titleAlignment,
+                        titleColor: this.titleColor,
+                        chartSubtitle: this.chartSubtitle,
+                        subtitleSize: this.subtitleSize,
+                        subtitleFontStyle: this.subtitleFontStyle,
+                        subtitleAlignment: this.subtitleAlignment,
+                        subtitleColor: this.subtitleColor,
+                        scaleFormat: this.scaleFormat,
+                        decimalPlaces: this.decimalPlaces
+                    }
+                }
+            }));
+        }
+
+        // Getters and setters for each property
+
+        // Font properties
+        get chartTitle() {
+            return this._shadowRoot.getElementById('chartTitle').value;
+        }
+        
+        set chartTitle(value) {
+            this._shadowRoot.getElementById('chartTitle').value = value;
+        }
+
+        get titleSize() {
+            return this._shadowRoot.getElementById('titleSize').value;
+        }
+
+        set titleSize(value) {
+            this._shadowRoot.getElementById('titleSize').value = value;
+        }
+
+        get titleFontStyle() {
+            return this._shadowRoot.getElementById('titleFontStyle').value;
+        }
+
+        set titleFontStyle(value) {
+            this._shadowRoot.getElementById('titleFontStyle').value = value;
+        }
+
+        get titleAlignment() {
+            return this._shadowRoot.getElementById('titleAlignment').value;
+        }
+
+        set titleAlignment(value) {
+            this._shadowRoot.getElementById('titleAlignment').value = value;
+        }
+
+        get titleColor() {
+            return this._shadowRoot.getElementById('titleColor').value;
+        }
+
+        set titleColor(value) {
+            this._shadowRoot.getElementById('titleColor').value = value;
+        }
+
+        get chartSubtitle() {
+            return this._shadowRoot.getElementById('chartSubtitle').value;
+        }
+
+        set chartSubtitle(value) {
+            this._shadowRoot.getElementById('chartSubtitle').value = value;
+        }
+
+        get subtitleSize() {
+            return this._shadowRoot.getElementById('subtitleSize').value;
+        }
+
+        set subtitleSize(value) {
+            this._shadowRoot.getElementById('subtitleSize').value = value;
+        }
+
+        get subtitleFontStyle() {
+            return this._shadowRoot.getElementById('subtitleFontStyle').value;
+        }
+
+        set subtitleFontStyle(value) {
+            this._shadowRoot.getElementById('subtitleFontStyle').value = value;
+        }
+
+        get subtitleAlignment() {
+            return this._shadowRoot.getElementById('subtitleAlignment').value;
+        }
+
+        set subtitleAlignment(value) {
+            this._shadowRoot.getElementById('subtitleAlignment').value = value;
+        }
+
+        get subtitleColor() {
+            return this._shadowRoot.getElementById('subtitleColor').value;
+        }
+
+        set subtitleColor(value) {
+            this._shadowRoot.getElementById('subtitleColor').value = value;
+        }
+
+        // Number formatting properties
+        get scaleFormat() {
+            return this._shadowRoot.getElementById('scaleFormat').value;
+        }
+
+        set scaleFormat(value) {
+            this._shadowRoot.getElementById('scaleFormat').value = value;
+        }
+
+        get decimalPlaces() {
+            return this._shadowRoot.getElementById('decimalPlaces').value;
+        }
+
+        set decimalPlaces(value) {
+            this._shadowRoot.getElementById('decimalPlaces').value = value;
+        }
+    }
+    customElements.define('com-sap-sample-sunburst-aps', SunburstAps);
+})();
---
+++ src/index.js
@@ -0,0 +1,244 @@
+import * as Highcharts from 'highcharts';
+import 'highcharts/modules/sunburst.js';
+
+/**
+ * Parses metadata into structured dimensions and measures.
+ * @param {Object} metadata - The metadata object from SAC data binding.
+ * @returns {Object} An object containing parsed dimensions, measures, and their maps.
+ */
+var parseMetadata = metadata => {
+    const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata;
+    const dimensions = [];
+    for (const key in dimensionsMap) {
+        const dimension = dimensionsMap[key];
+        dimensions.push({ key, ...dimension });
+    }
+
+    const measures = [];
+    for (const key in measuresMap) {
+        const measure = measuresMap[key];
+        measures.push({ key, ...measure });
+    }
+    return { dimensions, measures, dimensionsMap, measuresMap };
+}
+
+(function () {
+    class Sunburst extends HTMLElement {
+        constructor() {
+            super();
+            this.attachShadow({ mode: 'open' });
+
+            // Create a CSSStyleSheet for the shadow DOM
+            const sheet = new CSSStyleSheet();
+            sheet.replaceSync(`
+                @font-face {
+                    font-family: '72';
+                    src: url('../fonts/72-Regular.woff2') format('woff2');
+                }
+                #container {
+                    width: 100%;
+                    height: 100%;
+                    font-family: '72';
+                }
+            `);
+
+            // Apply the stylesheet to the shadow DOM
+            this.shadowRoot.adoptedStyleSheets = [sheet];
+
+            // Add the container for the chart
+            this.shadowRoot.innerHTML = `
+                <div id="container"></div>    
+            `;
+        }
+
+        /**
+         * Called when the widget is resized.
+         * @param {number} width - New width of the widget.
+         * @param {number} height - New height of the widget.
+         */
+        onCustomWidgetResize(width, height) {
+            this._renderChart();
+        }
+
+        /**
+         * Called after widget properties are updated.
+         * @param {Object} changedProperties - Object containing changed attributes.
+         */
+        onCustomWidgetAfterUpdate(changedProperties) {
+            this._renderChart();
+        }
+
+        /**
+         * Called when the widget is destroyed. Cleans up chart instance.
+         */
+        onCustomWidgetDestroy() {
+            if (this._chart) {
+                this._chart.destroy();
+                this._chart = null;
+            }
+        }
+
+        /**
+         * Specifies which attributes should trigger re-rendering on change.
+         * @returns {string[]} An array of observed attribute names.
+         */
+        static get observedAttributes() {
+            return [
+                'chartTitle', 'titleSize', 'titleFontStyle', 'titleAlignment', 'titleColor',                // Title properties
+                'chartSubtitle', 'subtitleSize', 'subtitleFontStyle', 'subtitleAlignment', 'subtitleColor', // Subtitle properties
+                'scaleFormat', 'decimalPlaces'                                                              // Number formatting properties
+            ];
+        }
+
+        /**
+        * Called when an observed attribute changes.
+        * @param {string} name - The name of the changed attribute.
+        * @param {string} oldValue - The old value of the attribute.
+        * @param {string} newValue - The new value of the attribute.
+        */
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (oldValue !== newValue) {
+                this[name] = newValue;
+                this._renderChart();
+            }
+        }
+
+        /**
+         * Renders the chart using the provided data and metadata.
+         */
+        _renderChart() {
+            const dataBinding = this.dataBinding;
+            if (!dataBinding || dataBinding.state !== 'success' || !dataBinding.data || dataBinding.data.length === 0) {
+                if (this._chart) {
+                    this._chart.destroy();
+                    this._chart = null;
+                }
+                return;
+            }
+            console.log('dataBinding:', dataBinding);
+            const { data, metadata } = dataBinding;
+            const { dimensions, measures } = parseMetadata(metadata);
+
+            if (dimensions.length === 0 || measures.length === 0) {
+                if (this._chart) {
+                    this._chart.destroy();
+                    this._chart = null;
+                }
+                return;
+            }
+
+            const [dimension] = dimensions;
+            const [measure] = measures;
+
+            const seriesData = [];
+
+            if (showAll) {
+                data = appendSuperRoot(data, labelAll);
+            }
+
+            data.forEach(row => {
+                const { id, label, parentId } = row[dimension.key];
+                const { raw } = row[measure.key];
+
+                seriesData.push({ 
+                    id: id,
+                    parent: parentId || '',
+                    name: label,
+                    value: raw
+                });
+            });
+
+            console.log('seriesData:', seriesData);
+
+            const scaleFormat = (value) => this._scaleFormat(value);
+            const subtitleText = this._updateSubtitle();
+
+            const chartOptions = {
+                chart: {
+                    type: 'sunburst',
+                    style: {
+                        fontFamily: "'72', sans-serif"
+                    }
+                },
+                title: {
+                    text: this.chartTitle || "",
+                    align: this.titleAlignment || "left",
+                    style: {
+                        fontSize: this.titleSize || "20px",
+                        fontWeight: this.titleFontStyle || "bold",
+                        color: this.titleColor || "#333333",
+                    },
+                },
+                subtitle: {
+                    text: subtitleText,
+                    align: this.subtitleAlignment || "left",
+                    style: {
+                        fontSize: this.subtitleSize || "12px",
+                        fontStyle: this.subtitleFontStyle || "normal",
+                        color: this.subtitleColor || "#666666",
+                    },
+                },
+                series: [{
+                    type: 'sunburst',
+                    data: seriesData,
+                    allowDrillToNode: true
+                }]
+            };
+            this._chart = Highcharts.chart(this.shadowRoot.getElementById('container'), chartOptions);
+        }
+
+        /**
+         * Determines subtitle text based on scale format or user input.
+         * @returns {string} The subtitle text.
+         */
+        _updateSubtitle() {
+            if (!this.chartSubtitle || this.chartSubtitle.trim() === '') {
+                let subtitleText = '';
+                switch (this.scaleFormat) {
+                    case 'k':
+                        subtitleText = 'in k';
+                        break;
+                    case 'm':
+                        subtitleText = 'in m';
+                        break;
+                    case 'b':
+                        subtitleText = 'in b';
+                        break;
+                    default:
+                        subtitleText = '';
+                        break;
+                }
+                return subtitleText;
+            } else {
+                return this.chartSubtitle;
+            }
+        }
+
+        _scaleFormat(value) {
+            let scaledValue = value;
+            let valueSuffix = '';
+
+            switch (this.scaleFormat) {
+                case 'k':
+                    scaledValue = value / 1000;
+                    valueSuffix = 'k';
+                    break;
+                case 'm':
+                    scaledValue = value / 1000000;
+                    valueSuffix = 'm';
+                    break;
+                case 'b':
+                    scaledValue = value / 1000000000;
+                    valueSuffix = 'b';
+                    break;
+                default:
+                    break;
+            }
+            return {
+                scaledValue: scaledValue.toFixed(this.decimalPlaces),
+                valueSuffix
+            };
+        }
+    }
+    customElements.define('com-sap-sample-sunburst', Sunburst);
+})();
