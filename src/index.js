0810d15bb5ff44b2593f38343fe4a6ce0c1fb1ce
Belinda <bmdebruyn@gmail.com>
2025-06-06 15:17:22 -0600

    change order of colors

--- src/index.js
+++ src/index.js
@@ -198,7 +198,7 @@ var parseMetadata = metadata => {
                 lang: {
                     thousandsSep: ','
                 },
-                colors: ['#004b8d', '#939598', '#00aa7e', '#faa834', '#47a5dc', '#006ac7', '#ccced2', '#bf8028', '#00e4a7']
+                colors: ['#004b8d', '#faa834', '#00aa7e', '#47a5dc', '#006ac7', '#ccced2', '#bf8028', '#00e4a7', '#939598']
             });
 
             const chartOptions = {
